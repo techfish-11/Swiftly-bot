511e38c8e07484b70ac78760cc54168e7493a654
techfish-11 <msuke1018@icloud.com>
2025-01-06 16:57:36 +0900

    Prophet成長予測機能の未来データフレームの期間を181日に変更

--- cogs/prophet-growth.py
+++ cogs/prophet-growth.py
@@ -32,7 +32,7 @@ class ProphetGrowth(commands.Cog):
             model = Prophet()
             model.fit(df)
 
-            future = model.make_future_dataframe(periods=365)
+            future = model.make_future_dataframe(periods=181)
             forecast = model.predict(future)
 
             found_date = None
