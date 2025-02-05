5eb7d26ec7b08075ee1ca0d8d0b51b316c2e683c
Moca-Love <raimu_hero@yahoo.co.jp>
2025-02-05 09:02:39 +0900

    Add write func

--- cogs/e_welcome.py
+++ cogs/e_welcome.py
@@ -2,35 +2,33 @@ import discord
 from discord.ext import commands
 import yaml
 
-# Load configuration from evex.yml
-with open('evex.yml', 'r', encoding='utf-8') as file:
-    config = yaml.safe_load(file)
-
-GUILD_ID = config['GUILD_ID']
-CHANNEL_ID = config['CHANNEL_ID']
-TARGET_MEMBER_COUNT = config['TARGET_MEMBER_COUNT']
 
 class MemberWelcomeCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.config = self._read_yml()
+
+        self.GUILD_ID = self.config["GUILD_ID"]
+        self.CHANNEL_ID = self.config["CHANNEL_ID"]
+        self.TARGET_MEMBER_COUNT = self.config["TARGET_MEMBER_COUNT"]
 
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
-        if member.guild.id != GUILD_ID:
+        if member.guild.id != self.GUILD_ID:
             return
 
-        guild = self.bot.get_guild(GUILD_ID)
-        channel = guild.get_channel(CHANNEL_ID)
+        guild = self.bot.get_guild(self.GUILD_ID)
+        channel = guild.get_channel(self.CHANNEL_ID)
 
         if guild is not None and channel is not None:
-            remaining_members = TARGET_MEMBER_COUNT - len(guild.members)
+            remaining_members = self.TARGET_MEMBER_COUNT - len(guild.members)
             if remaining_members > 0:
                 embed = discord.Embed(
                     title="ようこそ！",
                     description=(
                         f"{member.mention} さん、ようこそ！\n"
                         f"現在のメンバー数: {len(guild.members)}人。\n"
-                        f"あと {remaining_members} 人で {TARGET_MEMBER_COUNT}人達成です！"
+                        f"あと {remaining_members} 人で {self.TARGET_MEMBER_COUNT}人達成です！"
                     ),
                     color=discord.Color.green()
                 )
@@ -46,20 +44,33 @@ class MemberWelcomeCog(commands.Cog):
                 await channel.send(embed=embed)
 
         # 1000人超えたらお祝い
-        if len(guild.members) >= TARGET_MEMBER_COUNT:
+        if len(guild.members) >= self.TARGET_MEMBER_COUNT:
             await self.celebrate_1000_members(guild, channel)
 
     async def celebrate_1000_members(self, guild, channel):
         embed = discord.Embed(
             title="🎉🎉🎉 お祝い 🎉🎉🎉",
             description=(
-                f"{TARGET_MEMBER_COUNT}人達成！\n"
-                f"{guild.name}のメンバーが{TARGET_MEMBER_COUNT}人になりました！皆さんありがとうございます！"
+                f"{self.TARGET_MEMBER_COUNT}人達成！\n"
+                f"{guild.name}のメンバーが{self.TARGET_MEMBER_COUNT}人になりました！皆さんありがとうございます！"
             ),
             color=discord.Color.gold()
         )
         embed.set_footer(text="Hosted by techfish")
         await channel.send(embed=embed)
+
+    def _read_yml(self):
+        """ Load configuration from evex.yml """
+        with open("evex.yml", "r", encoding="utf-8") as file:
+            config = yaml.safe_load(file)
+
+        return config
+
+    def _write_yml(self):
+        """ Save configuration from evex.yml """
+        with open("evex.yml", "w", encoding="utf-8") as file:
+            yaml.safe_dump(self.config, file, allow_unicode=True)
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(MemberWelcomeCog(bot))
