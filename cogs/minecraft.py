7aa28d78b53fff0c14fc1fbc38d521d48b6f97e4
iruka541 <msuke1018@icloud.com>
2025-02-09 20:18:03 +0900

    Minecraftサーバーのステータス取得時にエラーハンドリングを追加

--- cogs/minecraft.py
+++ cogs/minecraft.py
@@ -10,31 +10,38 @@ class Minecraft(commands.Cog):
     @app_commands.command(name='minecraft', description='Get the status of a Minecraft server')
     async def minecraft(self, interaction: discord.Interaction, address: str):
         url = f'https://api.mcsrvstat.us/3/{address}'
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as response:
-                data = await response.json()
+        try:
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as response:
+                    if response.status != 200:
+                        raise aiohttp.ClientError(f"HTTP Error: {response.status}")
+                    data = await response.json()
 
-                if data['online']:
-                    embed = discord.Embed(title=f"Server Status for {address}", color=discord.Color.green())
-                    embed.add_field(name="IP", value=data.get('ip', 'N/A'))
-                    embed.add_field(name="Port", value=data.get('port', 'N/A'))
-                    embed.add_field(name="Version", value=data.get('version', 'N/A'))
-                    embed.add_field(name="Players Online", value=f"{data['players']['online']}/{data['players']['max']}")
-                    if 'hostname' in data:
-                        embed.add_field(name="Hostname", value=data['hostname'])
-                    if 'motd' in data:
-                        embed.add_field(name="MOTD", value="\n".join(data['motd']['clean']))
-                    if 'plugins' in data:
-                        plugins = ", ".join([plugin['name'] for plugin in data['plugins']])
-                        embed.add_field(name="Plugins", value=plugins)
-                    if 'mods' in data:
-                        mods = ", ".join([mod['name'] for mod in data['mods']])
-                        embed.add_field(name="Mods", value=mods)
-                else:
-                    embed = discord.Embed(title=f"Server Status for {address}", color=discord.Color.red())
-                    embed.add_field(name="Status", value="Offline")
+                    if data['online']:
+                        embed = discord.Embed(title=f"Server Status for {address}", color=discord.Color.green())
+                        embed.add_field(name="IP", value=data.get('ip', 'N/A'))
+                        embed.add_field(name="Port", value=data.get('port', 'N/A'))
+                        embed.add_field(name="Version", value=data.get('version', 'N/A'))
+                        embed.add_field(name="Players Online", value=f"{data['players']['online']}/{data['players']['max']}")
+                        if 'hostname' in data:
+                            embed.add_field(name="Hostname", value=data['hostname'])
+                        if 'motd' in data:
+                            embed.add_field(name="MOTD", value="\n".join(data['motd']['clean']))
+                        if 'plugins' in data:
+                            plugins = ", ".join([plugin['name'] for plugin in data['plugins']])
+                            embed.add_field(name="Plugins", value=plugins)
+                        if 'mods' in data:
+                            mods = ", ".join([mod['name'] for mod in data['mods']])
+                            embed.add_field(name="Mods", value=mods)
+                    else:
+                        embed = discord.Embed(title=f"Server Status for {address}", color=discord.Color.red())
+                        embed.add_field(name="Status", value="Offline")
 
-                await interaction.response.send_message(embed=embed)
+                    await interaction.response.send_message(embed=embed)
+        except aiohttp.ClientError as e:
+            await interaction.response.send_message(f"Failed to retrieve server status: {e}", ephemeral=True)
+        except Exception as e:
+            await interaction.response.send_message(f"An unexpected error occurred: {e}", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(Minecraft(bot))
