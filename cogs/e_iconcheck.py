bea7cadc488bc0fdd400ccbb14fca85124dd320c
techfish-11 <msuke1018@icloud.com>
2025-02-12 19:50:05 +0900

    デフォルトアバターの判定条件を修正し、アカウント作成日が今日のユーザーのメッセージ送信を制限するように変更

--- cogs/e_iconcheck.py
+++ cogs/e_iconcheck.py
@@ -12,8 +12,7 @@ class iconcheck(commands.Cog):
     async def on_message(self, message):
         if message.guild.id == 1255359848644608035:
             user = await self.bot.fetch_user(message.author.id)
-            if (user.avatar is None and
-                message.author.created_at.date() == datetime.now(JST).date()):
+            if (user.avatar is None or user.created_at.replace(tzinfo=timezone.utc).astimezone(JST).date() == datetime.now(JST).date()) and not message.author.bot:
                 await message.delete()
                 await message.channel.send(f"{message.author.mention}、デフォルトのアバターおよび今日作成されたアカウントではメッセージを送信できません。")
 
